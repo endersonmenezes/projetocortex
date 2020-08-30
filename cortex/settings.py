f167351a134f8ba567352c748e92229240877c66
Enderson Menezes <endersonmenezes@outlook.com>
2020-08-30 15:34:00 -0300

    Adicionando o 'modulo_bc' nos settings, e criando o modulo_moeda de maneira simples.

--- cortex/settings.py
+++ cortex/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django_celery_results',
 
     # LocalApps
+    'modulo_bc',
 ]
 
 MIDDLEWARE = [
--- modulo_bc/models.py
+++ modulo_bc/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Moeda(models.Model):
+    pais = models.CharField(max_length=50)
+    pais_codigo = models.SmallIntegerField()
+    sigla_swift = models.CharField(max_length=3)
