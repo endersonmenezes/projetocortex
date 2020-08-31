318f7a4992496143197b28f36e0371d697a3d4ad
Enderson Menezes <endersonmenezes@outlook.com>
2020-08-30 21:06:33 -0300

    Validando os dados.

--- modulo_bc/views.py
+++ modulo_bc/views.py
@@ -8,8 +8,7 @@ import dateutil.parser
 from datetime import datetime
 from django.utils import timezone
 from .models import Moeda
-import json
-import xmltodict
+from django.core.cache import cache
 from .xml_dict import get_json_from_xml
 from django.views.decorators.cache import cache_page
 
@@ -18,7 +17,7 @@ from django.views.decorators.cache import cache_page
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-@cache_page(60 * 30)  # Cache de 30 minutos
+@cache_page(60 * 30, key_prefix='converter_moedas')  # Cache de 30 minutos
 def converter_moedas(request):
     """
     API de CoGnversão das Moedas
@@ -34,17 +33,38 @@ def converter_moedas(request):
 
     Exemplo de Chamada: /?moeda_origem=BRL&moeda_destino=USD&valor_desejado=10&data_cotacao=10/09/2019
     """
-    data = dict()
     data_cotacao = request.GET.get('data_cotacao', None)
     moeda_origem = request.GET.get('moeda_origem', None)
     moeda_destino = request.GET.get('moeda_destino', None)
     valor_desejado = request.GET.get('valor_desejado', None)
     if data_cotacao and moeda_origem and moeda_destino and valor_desejado:
-        moeda_origem = Moeda.objects.get(moeda_swift=moeda_origem).moeda_codigo
-        moeda_destino = Moeda.objects.get(moeda_swift=moeda_destino).moeda_codigo
-        data_cotacao = datetime.strptime(data_cotacao, '%d/%m/%Y').replace(tzinfo=timezone.get_current_timezone())
-        data_cotacao_str = data_cotacao.strftime('%Y-%m-%d')
-        # TODO Validar os argumentos inseridos
+        # Testar e validar moeda_origem
+        try:
+            moeda_origem = Moeda.objects.get(moeda_swift=moeda_origem).moeda_codigo
+        except Moeda.DoesNotExist:
+            raise ValidationError(detail='Código da moeda de origem incorreto.')
+
+        # Testar e validar moeda de destino
+        try:
+            moeda_destino = Moeda.objects.get(moeda_swift=moeda_destino).moeda_codigo
+        except Moeda.DoesNotExist:
+            raise ValidationError(detail='Código da moeda de destino incorreto.')
+
+        # Testar a data digitada
+        try:
+            data_cotacao = datetime.strptime(data_cotacao, '%d/%m/%Y').replace(tzinfo=timezone.get_current_timezone())
+            data_cotacao_str = data_cotacao.strftime('%Y-%m-%d')
+        except ValueError:
+            raise ValidationError(detail='Verifique a data desejada se foi digitada no padrão correto.')
+
+        # Testar valor desejado
+        try:
+            valor_desejado: str = valor_desejado.replace(',', '.')
+            valor_desejado: float = float(valor_desejado)
+        except ValueError:
+            raise ValidationError(detail='Verifique o valor desejado, se está nos padrões.')
+
+
 
         # ---------------------------------------------
         # Temos que verificar primeiro se a data solicitada existe cotação.
@@ -61,7 +81,6 @@ def converter_moedas(request):
 
         # ---------------------------------------------
         # Vamos converter o valor desejado agora
-        valor_desejado: str = valor_desejado.replace(',', '.')
         url = 'https://www3.bcb.gov.br/bc_moeda/rest/converter/{}/1/{}/{}/{}'.format(
             valor_desejado,
             moeda_origem,
